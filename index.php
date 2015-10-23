53ad32926936ba13818616007950654ce880c894
Mike Vartanian <mike.vartanian@gmail.com>
2015-10-23 07:59:25 -0400

    Added lots of text to index page...

--- index.php
+++ index.php
@@ -27,6 +27,28 @@
       </div>
     </div>
     <div class="mikepic">
+      <h1>My Basic Problem</h1>
+      <p>So here is the basic story... I have loaned out way too much money to my friends and it has become very difficult for
+      me to keep track of who owes me what... I shouldn't be loaning out this money but I feel bad and am a gigantic sucker...
+      Any sob story you tell me can extract any amount of money from me... I have tried to work on this but have not been able
+      to correct this non-productive behavior of mine...</p>
+      <p>With all of today's technology I figured there should be an easy way for me to keep better track of the money that is owed
+        to me as well as facilitate methods for more convenient payment methods (both for me and the person that owes me money!!)</p>
+      <h1>How did I do this before?</h1>
+      <p>Lending out money and attempting to collect in the past has been a very difficult and frustrating process to say the least.</p>
+      <p>I have used Excel, Quicken, Word, Email, and paper slips to track how much money I have loaned out to who and when... I would
+        xerox checks that were sent to me and provide paper receipts for those who paid with cash... Needless to say that this system
+        became very cumbersome and any lapse on my part (ie forgetting to document a loan or payment), dramatically increased the complexity
+        of my system...</p>
+      <h1>The Solution(s)...</h1>
+      <p>With cheap cloud computing and storage, ubuiquity of smart mobile devices with strong application ecosystems, and the 
+        skills that I have gathered in my web development classes, I have put together a full payment solution that 
+        significantly reduces the stress and issues associated with loans and payment collection.</p>
+      <p> It turns out that this solution can be used by others as well and with the increased use of normalized APIs,
+          I can connect my solution with other systems and infrastructure such as payment, workforce, and asset tracking 
+          solutions, both new and legacy.</p>
+          <h1>Is this for you?</h1>
+          <p>Well if you owe me money, you have to use this...:) If you want to borrow money from me, you have to use this...:)
     </div>
 </body>
 </html>
