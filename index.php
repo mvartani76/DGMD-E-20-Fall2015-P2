2723ae07f4a5339ba58c462abdd31687a78b7d94
Mike Vartanian <mike.vartanian@gmail.com>
2015-10-23 13:27:51 -0400

    Updated index source and stylings for images..

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <link href="./css/services.css" rel="stylesheet">
   <link href="./css/footer.css" rel="stylesheet">  
   <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
@@ -27,19 +28,35 @@
       </div>
     </div>
     <div class="messydeskpic">
+      <div class="storyandsolution">
       <h1>My Basic Problem</h1>
-      <p>So here is the basic story... I have loaned out way too much money to my friends and it has become very difficult for
-      me to keep track of who owes me what... I shouldn't be loaning out this money but I feel bad and am a gigantic sucker...
-      Any sob story you tell me can extract any amount of money from me... I have tried to work on this but have not been able
+      <img src="./images/people-grabbing-money-from-businessman.jpg" class="greedyhandspic">
+      <img src="./images/greedy-man-asking-for-money.jpg" class="greedyhandspic">
+      <p>So here is the basic story... The abbreviated version of course...:)</p>
+      <p>I have loaned out way too much money to my friends and it has become very difficult for
+      me to keep track of who owes me what... I shouldn't be loaning out this money but I feel bad and am a gigantic sucker...</p>
+      <p class="clear">&nbsp;</p>
+      <img src="./images/woman-asking-for-money.jpg" class="womanaskingformoneypic">
+      <p>Any sob story you tell me can extract any amount of money from me...</p>
+      <p> <em>"I cannot pay you rent because I forgot about my
+        insurance payment..." "I will get you your money next week..."</em>
+      <p>I have tried to work on this but have not been able
       to correct this non-productive behavior of mine...</p>
+      <p class="clear">&nbsp;</p>
+      <img src="./images/man-pulling-hair-out.jpg" class="manpullinghairoutpic">
       <p>With all of today's technology I figured there should be an easy way for me to keep better track of the money that is owed
         to me as well as facilitate methods for more convenient payment methods (both for me and the person that owes me money!!)</p>
+
+
+        <p class="clear">&nbsp;</p>
       <h1>How did I do this before?</h1>
+      <img src="./images/paper-on-desk.jpg" class="paperondeskpic">
       <p>Lending out money and attempting to collect in the past has been a very difficult and frustrating process to say the least.</p>
       <p>I have used Excel, Quicken, Word, Email, and paper slips to track how much money I have loaned out to who and when... I would
-        xerox checks that were sent to me and provide paper receipts for those who paid with cash... Needless to say that this system
-        became very cumbersome and any lapse on my part (ie forgetting to document a loan or payment), dramatically increased the complexity
+        xerox checks that were sent to me and provide paper receipts for those who paid with cash...</p>
+        <p>Needless to say that this system became very cumbersome and any lapse on my part (ie forgetting to document a loan or payment), dramatically increased the complexity
         of my system...</p>
+        <p class="clear">&nbsp;</p>
       <h1>The Solution(s)...</h1>
       <p>With cheap cloud computing and storage, ubuiquity of smart mobile devices with strong application ecosystems, and the 
         skills that I have gathered in my web development classes, I have put together a full payment solution that 
@@ -48,7 +65,9 @@
           I can connect my solution with other systems and infrastructure such as payment, workforce, and asset tracking 
           solutions, both new and legacy.</p>
           <h1>Is this for you?</h1>
+          <img src="./images/mikepic.png" class="mikepic">
           <p>Well if you owe me money, you have to use this...:) If you want to borrow money from me, you have to use this...:)
+      </div>
     </div>
 </body>
 </html>
